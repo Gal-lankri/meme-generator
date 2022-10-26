5e652f930e08a1318f92d8bf965ea8a2f2dc1c59
Gal Lankri <gal.lankri2@gmail.com>
2022-10-26 11:00:21 +0300

    first upload

---
+++ js/services/meme.service.js
@@ -0,0 +1,8 @@
+'use strict'
+const gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gMeme = {
+  selectedImgId: 5,
+  selectedLineIdx: 0,
+  lines: [{ txt: 'I sometimes eat Falafel', size: 20, align: 'left', color: 'red' }],
+}
